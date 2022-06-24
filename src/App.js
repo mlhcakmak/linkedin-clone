379cad7fd379b417e98de53847f6aba4093c6fd2
sleent67 <57071691+sleent67@users.noreply.github.com>
2022-06-24 16:50:08 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Login from './components/login';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Router>
         <Routes>
         <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/home" element={<><Header/><Home/></>}/>
         </Routes>
       </Router>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Header=(props) => {
+return(
+<Container>
+    <Content>Header</Content>
+</Container>
+);
+};
+
+const Container=styled.div`
+background-color: white;
+border-bottom: 1px solid rgba(0,0,0,0.08);
+left: 0;
+padding: 0 24px;
+top: 0;
+position: fixed;
+width: 100vw;
+z-index: 100;
+`;
+
+const Content=styled.div`
+display: flex;
+align-items: center;
+margin: 0 auto;
+min-height: 100%;
+max-width: 1128px;
+`;
+export default Header;
